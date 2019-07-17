daf800599e702f334d7d9e0d00f8fd56e88e46aa
Hudd <haddayn@gmail.com>
2019-07-17 12:53:19 +0300

    Parse command line parameters for server

--- server/server_main.cpp
+++ server/server_main.cpp
@@ -1,17 +1,34 @@
 #include <server/server.h>
 
-#include <thread>
 #include <iostream>
 #include <stdexcept>
 
 #include <signal.h>
 
-int main(int, char** argv)
+
+int main(int argc, char** argv)
 {
-	signal(SIGPIPE, SIG_IGN);
-	
+	// TODO: I didn't have time to search for good options parser
+	if (argc < 2) {
+		std::cerr << "USAGE: server PORT [ADDRESS]\n";
+		return EXIT_FAILURE;
+	}
+
+	std::uint16_t port = 0;
+	aw::ip4_address addr = 0;
 	try {
-		aw::server srv(0, 10000);
+		port = aw::parse_port(argv[1]);
+		if (argc >= 3)
+			addr = aw::parse_address(argv[2]);
+	} catch(std::exception& ex) {
+		std::cerr << "error: " << ex.what() << '\n';
+		return EXIT_FAILURE;
+	}
+
+	signal(SIGPIPE, SIG_IGN);
+
+	try {
+		aw::server srv(addr, port);
 		srv.run();
 	} catch( std::exception& ex ) {
 		std::cerr << "Unrecoverable error occured:\n";
